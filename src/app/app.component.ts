8c3b09a232c55f685dbdebb77742832f79aa0702
George I Orhewere <george.orhewere@gmail.com>
2019-02-14 21:26:43 +0100

    exploring models and repositories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div class="bg-success p-2 text-center text-white">
+      This is SportsStore
+  </div>`
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app-root';
 }
